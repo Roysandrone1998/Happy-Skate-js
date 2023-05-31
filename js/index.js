63654dafd015136066fc9380416b056ca60160ea
Roysandrone1998 <raysandrone@gmail.com>
2023-05-30 23:08:32 -0300

    Primer entrega terminada

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-
+// Consola de comandos 
 let comando;
 
 while (comando !== 'Salir') {
-    comando = prompt('Ingrese el comando:\n\n- Productos\n- Mostrar cuotas\n- Contactanos\n- Salir');
-    
+    comando = prompt('Ingrese el comando:\n\n- Color filamentos\n- Mostrar cuotas\n- Contactanos\n- Salir');
+
     switch (comando) {
-        case 'Productos':
+        case 'Color filamentos':
             let colorProducto = prompt('Ingrese el color\n\n- Negro\n- Gris espacial\n- Bronce');
             if (colorProducto === "Negro") {
                 alert("El color " + colorProducto + " está en stock.");
@@ -14,13 +14,14 @@ while (comando !== 'Salir') {
             } else if (colorProducto === "Bronce") {
                 alert("El color '" + colorProducto + "' está en stock.");
             } else {
-                alert("No hay ese color en stock");
+                alert("No hay ese color en stock.");
             }
             break;
-            case 'Mostrar cuotas':
-                let cuotas = 1;
-                for (let i = 0; i < 3; i++) {
-                    alert('Puedes pagar en ' + cuotas + ' cuotas sin interés');
+
+        case 'Mostrar cuotas':
+            let cuotas = 1;
+            for (let i = 0; i < 3; i++) {
+                alert('Puedes pagar en ' + cuotas + ' cuotas sin interés.');
                 if (i === 0) {
                     cuotas += 2;
                 } else {
@@ -28,11 +29,13 @@ while (comando !== 'Salir') {
                 }
             }
             break;
-            case 'Contactanos':
-                let nombre = prompt("Ingresa tu nombre.");
-                let mail = prompt("Ingresa tu mail.");
-                let mensaje = prompt("Dejanos tu mensaje o pedido.");
-                alert("Hola " + nombre + ", tu mail es: " + mail + ". Confirma y estaremos en contacto contigo lo antes posible.");
-                break;
-        }
+
+        case 'Contactanos':
+            let nombre = prompt("Ingresa tu nombre.");
+            let mail = prompt("Ingresa tu mail.");
+            let mensaje = prompt("Dejanos tu mensaje o pedido.");
+            alert("Hola " + nombre + ", tu mail es: " + mail);
+            continuar = confirm("Confirma y estaremos en contacto contigo lo antes posible.")
+            break;
+    }
 }
